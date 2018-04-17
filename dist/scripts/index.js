0cb7084f6315178337d131041d79d5bd706637ac
Jonathan Kuhl <prj.breakpoint@gmail.com>
2018-04-16 23:30:14 -0400

    Add files via upload
    Added a few pages, some javascript

---
+++ dist/scripts/index.js
@@ -0,0 +1,27 @@
+!function() {
+
+    const cards = Array.from(document.querySelectorAll('.cardholder .card'));
+    const techs = Array.from(document.querySelectorAll('.techstack img'));
+
+    // cards.forEach( (card)=> console.log(card.dataset.tech));
+
+    function filterByTech(tech) {
+        return cards.filter((card)=> card.dataset.tech === tech);
+    }
+
+    function displayTech(tech) {
+        const cardholder = document.querySelector('.cardholder');
+        cardholder.innerHTML = '';
+        filterByTech(tech).forEach( (card)=> {
+            let div = document.createElement('div');
+            div.classList.add('card');
+            div.innerHTML = card.innerHTML;
+            cardholder.appendChild(div);
+        });
+    }
+
+    techs.forEach( (tech)=> tech.addEventListener('click', ()=> {
+        displayTech(tech.alt);
+    }));
+
+}();
