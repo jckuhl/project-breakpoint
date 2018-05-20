cb73e9d9f0314e725ce1e23dd523924fde68cc8c
Jonathan Kuhl <jonathankuhl@Jonathans-MBP.maine.rr.com>
2018-05-20 18:23:57 -0400

    set up repo

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -5,17 +5,23 @@
     const cards = Array.from(document.querySelectorAll('.cardholder .card'));
     const logos = document.querySelectorAll('.techstack figure');
     const cardholder = document.querySelector('.cardholder');
+    const typeSelect = document.getElementById('typeSelect');
+    let type = 'any';
 
     // returns an array of cards matching the selected technology
     function filterByTech(tech) {
-        return cards.filter( (card)=> card.dataset.tech.split(' ').includes(tech) );
+        const filtered = cards.filter( (card)=> card.dataset.tech.split(' ').includes(tech) );
+        if(type != 'any') {
+            return filtered.filter( (card)=> card.dataset.tech.split(' ').includes(type) );
+        }
+        return filtered;
     }
 
     // create the div elements for the cards
-    function createCards(card) {
+    function createCards(cardHTML) {
         const art = document.createElement('article');
         art.classList.add('card');
-        art.innerHTML = card.innerHTML;
+        art.innerHTML = cardHTML;
         cardholder.appendChild(art);
     }
 
@@ -23,16 +29,24 @@
     function displayTech(tech) {
         cardholder.innerHTML = '';
         if(tech === 'all') { 
-            cards.forEach( (card)=> createCards(card) );
+            cards.forEach( (card)=> createCards(card.innerHTML) );
         } else {
-            filterByTech(tech).forEach( (card)=> createCards(card) );
+            const filtered = filterByTech(tech);
+            if(filtered.length) {
+                filtered.forEach( (card)=> createCards(card.innerHTML) );
+            } else {
+                const cardHTML = `
+                    <h2>No matches</h2>
+                    <br>
+                    <p>Sorry, nothing was found that matched your filter.</p>`;
+                typeSelect.value = 'any';
+                createCards(cardHTML);
+            }
         }
     }
 
-    // add event listeners to the images in the tech filter
-    logos.forEach( (logo)=> logo.addEventListener('click', ()=> {
+    function choseTech(logo) {
         const logoImg = logo.children[0];
-        const technology = logo.dataset.tech;
         if(logoImg.classList.contains('deselected')) {
             // select the technology
             logoImg.classList.remove('deselected');
@@ -45,12 +59,28 @@
                     logo.children[0].classList.add('deselected');
                 }
             });
-            displayTech(technology);
+            displayTech(logo.dataset.tech);
         } else {
             logoImg.classList.remove('selected');
             logoImg.classList.add('deselected');
             displayTech('all');
         }
+    }
+
+    // add event listeners to the images in the tech filter
+    logos.forEach( (logo)=> logo.addEventListener('click', ()=> {
+        choseTech(logo);
     }));
+
+    // add change event listener to the type selector
+    typeSelect.addEventListener('change', ()=> {
+        type = typeSelect.value;
+        const imgs = Array.from(document.querySelectorAll('.techstack img'));
+        // grab the only selected image
+        const selected = imgs.filter( (img)=> img.classList.contains('selected'))[0];
+        if(selected) {
+            displayTech(selected.closest('figure').dataset.tech);
+        }
+    });
 
 })();
