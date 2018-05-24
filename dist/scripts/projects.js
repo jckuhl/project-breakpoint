3b8ab0c24f72e4f3ae38c573f7ca52d9076fe1a1
Jonathan Kuhl <jonathankuhl@164.sub-97-40-226.myvzw.com>
2018-05-24 14:43:42 -0400

    added web project site that fetches Github api

---
+++ dist/scripts/projects.js
@@ -0,0 +1,17 @@
+
+fetch('https://api.github.com/users/jckuhl/repos')
+    .then(data => data.json())
+    .then(data => {
+        const cardHolder = document.querySelector('.cardholder');
+        const cardData = data.filter(repo => !repo.fork && !repo.archived);
+        cardData.forEach(repo => {
+            const article = document.createElement('article');
+            article.classList.add('card');
+            article.innerHTML = `<h2>${repo.name.split('-').join(' ')}</h2>
+                <p><a href="${repo.html_url}" target="_blank">Github</a></p>
+                <p>${repo.description}</p>`;
+            cardHolder.appendChild(article);
+        });
+    })
+    .catch(error => console.error(data))
+
